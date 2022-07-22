2acfb88f433bd4efe8531b45951293dfc2963499
vignesh-vl <kirubhavikki@gmail.comm>
2022-07-22 18:25:41 +0530

    changes made @ 22/ 07

---
+++ static.java
@@ -0,0 +1,12 @@
+import java .util.*;
+class static
+{   
+    public static void main(String args [])
+    {
+      // static variables are constants
+      // shared by all the objects 
+      // static block (executes at the first time an object is created )
+      // we cannot use non static variables inside a static block 
+      
+    }
+}
