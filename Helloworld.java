e716532b4646c6545d88a863ef8029977704c5c4
vignesh-vl <kirubhavikki@gmail.comm>
2022-07-22 17:49:01 +0530

    first commit

---
+++ Helloworld.java
@@ -0,0 +1,10 @@
+import java .util.*;
+class Helloworld 
+{
+    public static void main(String args[])
+    {
+        Scanner scan  = new Scanner (System.in);
+        System.out.println("Hello world "); 
+        return;
+    }
+}
---
+++ varaiables.java
@@ -0,0 +1,15 @@
+public class varaiables {
+    
+    public static void main(String args [])
+    {
+      example obj =  new example();
+      System.out.println(obj.x);
+    }
+
+}
+class example 
+{
+    static int x = 100;
+    int  t =100;
+    
+}
