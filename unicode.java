ee5100a5e50976a103e93480a49c3eb4e46c34c9
vignesh-vl <kirubhavikki@gmail.comm>
2022-08-08 17:05:03 +0530

    java unicode

---
+++ unicode.java
@@ -0,0 +1,19 @@
+public class unicode {
+    // universal  internation standard encoding  that is capable of representing mmost of the worlds written languages 
+
+    /* PROBLEMS :
+
+     A particular code value corresponds to different letters in the various language standards.
+     The encodings for languages with large character sets have variable length.Some common characters are encoded as single bytes, other require two or more byte
+     
+     */
+
+    /* SOLUTION :  
+
+    To solve these problems, a new language standard was developed i.e. Unicode System.
+    In unicode, character holds 2 byte, so java also uses 2 byte for characters.
+    lowest value:\u0000
+    highest value:\uFFFF
+
+    */
+}
